9e0daadee0ff6aebd06f4db28a97ff80444ab59b
Zoe0714 <159541965+Zoe0714@users.noreply.github.com>
2025-02-25 10:54:37 -0300

    Add files via upload

--- Jogo_Adivinhacao.py
+++ Jogo_Adivinhacao.py
@@ -1,29 +1,38 @@
 print("°°°°°°°°°°°°°°°°°")
 print("Bem-Vindo ao Jogo\n")
 print("Acerte o número para ganhar!\n")
-print("Dica: É divisível por 7 e maior que 4x10.\n")
+print("Dica: É divisível por 7 e maior que 4 multiplicando 10.\n")
 print("°°°°°°°°°°°°°°°°°")
 
 numero_secreto = 77
-total_tentativas = 3
+total_tentativas = 8
+tentativas_tentadas = 1
 
 while (total_tentativas > 0):
+    print(f"Tentativa {tentativas_tentadas} de 8")
     chute = int(input("Digite o seu número: \n"))
 
     acertou = numero_secreto == chute
     chute_maior = chute > numero_secreto
     chute_menor = chute < numero_secreto
+    chute_proximo = chute 
 
     print(f"Você digitou {chute}\n")
+
+    total_tentativas = total_tentativas - 1
+    tentativas_tentadas = tentativas_tentadas + 1
 
     if(acertou):
         print("Você acertou!\n")
         print("°°°°°°°°°°°")
         print("Parabéns! Fim de Jogo")
         print("°°°°°°°°°°°")
+
     else:
         if(chute_maior):
             print("Humm... Seu chute foi maior que o número surpresa. Tente novamente!\n")
         elif(chute_menor):
             print("Humm... Seu chute foi menor que o número surpresa. Tente novamente!\n")
-  
+
+
+
