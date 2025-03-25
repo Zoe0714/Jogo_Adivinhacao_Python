87990f23a1c3dc7eae3c8d4da3740367b3e71e4d
Zoe0714 <159541965+Zoe0714@users.noreply.github.com>
2025-03-25 08:59:08 -0300

    Add files via upload

--- Jogo_Adivinhacao.py
+++ Jogo_Adivinhacao.py
@@ -1,19 +1,34 @@
+import random
+
 print("°°°°°°°°°°°°°°°°°")
 print("Bem-Vindo ao Jogo\n")
 print("Acerte o número para ganhar!\n")
-print("Dica: É divisível por 7 e maior que 4 multiplicando 10.\n")
 print("°°°°°°°°°°°°°°°°°")
 
-numero_secreto = 77
-total_tentativas = 9
+numero_secreto = round(random.randrange(50,80))
+pontos = 100
+
+print(numero_secreto)
+
+print("Qaul é o nível de dificuldade desejado?\n")
+print("(1) Fácil (2) Moderado (3) Difícil\n")
+
+nivel = int(input("Digite o nível desejado:\n "))
+
+if(nivel == 1):
+    total_tentativas = 20
+elif(nivel == 2):
+    total_tentativas = 10
+else:
+    total_tentativas = 5
 
 for rodadas in range (1,total_tentativas):
-    print(f"Tentativa {rodadas} de 8")
+    print(f"Tentativa {rodadas} de 8\n")
     chute = int(input("Digite um número entre 20 e 80: \n"))
     
 
-    if(chute < 20 or chute > 80):
-        print("Você deve digitar um número entre 20 e 80!")
+    if(chute < 50 or chute > 80):
+        print("Você deve digitar um número entre 50 e 80!")
         continue 
 
     acertou = numero_secreto == chute
@@ -26,10 +41,7 @@ for rodadas in range (1,total_tentativas):
     total_tentativas = total_tentativas - 1
 
     if(acertou):
-        print("Você acertou!\n")
-        print("°°°°°°°°°°°")
-        print("Parabéns! Fim de Jogo")
-        print("°°°°°°°°°°°")
+        print(f"Você acertou! Sua pontuação foi {pontos} pontos\n")
         break
 
     else:
@@ -37,3 +49,9 @@ for rodadas in range (1,total_tentativas):
             print("Humm... Seu chute foi maior que o número surpresa. Tente novamente!\n")
         elif(chute_menor):
             print("Humm... Seu chute foi menor que o número surpresa. Tente novamente!\n")
+            pontos_perdidos = abs(numero_secreto - chute)
+            pontos = pontos - pontos_perdidos
+
+print("°°°°°°°°°°°")
+print("Parabéns! Fim de Jogo")
+print("°°°°°°°°°°°")
