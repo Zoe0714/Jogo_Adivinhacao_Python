8322225a67e1f70c90aff1b5709e8c7d18bb0768
Zoe0714 <159541965+Zoe0714@users.noreply.github.com>
2025-04-01 08:38:31 -0300

    Add files via upload

--- Jogo_Adivinhacao.py
+++ Jogo_Adivinhacao.py
@@ -11,7 +11,7 @@ pontos = 100
 
 print(numero_secreto)
 
-print("Qaul é o nível de dificuldade desejado?\n")
+print("Qual é o nível de dificuldade desejado?\n")
 print("(1) Fácil (2) Moderado (3) Difícil\n")
 
 nivel = int(input("Digite o nível desejado:\n "))
@@ -23,23 +23,20 @@ elif(nivel == 2):
 else:
     total_tentativas = 5
 
-for rodadas in range (1,total_tentativas):
-    print(f"Tentativa {rodadas} de 8\n")
-    chute = int(input("Digite um número entre 20 e 80: \n"))
+for rodadas in range (1, total_tentativas + 1):
+    print(f"Tentativa {rodadas} de {total_tentativas}\n")
+    chute = int(input("Digite um número entre 50 e 80: \n"))
     
 
-    if(chute > 50 or chute < 80):
+    if(chute < 50 or chute > 80):
         print("Você deve digitar um número entre 50 e 80!")
         continue 
 
     acertou = numero_secreto == chute
     chute_maior = chute > numero_secreto
     chute_menor = chute < numero_secreto
-    chute_proximo = chute 
 
     print(f"Você digitou {chute}\n")
-
-    total_tentativas = total_tentativas - 1
 
     if(acertou):
         print(f"Você acertou! Sua pontuação foi {pontos} pontos\n")
@@ -47,12 +44,21 @@ for rodadas in range (1,total_tentativas):
         print("Parabéns! Fim de Jogo")
         print("°°°°°°°°°°°")
         break
-
     else:
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos -= pontos_perdidos
+
+        if pontos < 0:
+            pontos = 0
+
         if(chute_maior):
             print("Humm... Seu chute foi maior que o número surpresa. Tente novamente!\n")
         elif(chute_menor):
             print("Humm... Seu chute foi menor que o número surpresa. Tente novamente!\n")
-            pontos_perdidos = abs(numero_secreto - chute)
-            pontos = pontos - pontos_perdidos
 
+        print(f"Você perdeu {pontos_perdidos} pontos. Pontuação restante: {pontos}\n")
+
+if pontos > 0:
+    print("Fim de jogo. Essa foi sua pontuação:", pontos)
+else:
+    print("Fim de jogo. VOcê não pontuou.")
